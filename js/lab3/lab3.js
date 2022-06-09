33875704bad7ca447b8d4a4fcefeed7a3d6bba36
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-06-09 18:06:31 +0300

    Add files via upload

---
+++ js/lab3/lab3.js
@@ -0,0 +1,55 @@
+'use strict';
+function getDecimal(num) {
+    let a = num.slice(num.indexOf("."));
+    let str = 0 + a;
+    if (Number(num) < 0) {
+        str = 1 - Number(str);
+        str = str.toFixed(a.length - 1);
+    }
+    return str;
+}
+
+function ucFirst(str) {
+    	if (str.length==0) return '';
+    return (str[0].toUpperCase() + str.slice(1));
+}
+
+function checkSpam(str) {
+    str = str.toLowerCase();
+    if ((str.includes('xxx')) || (str.includes('viagra')))
+        return true;
+    else return false;
+}
+
+function truncate(str, maxlength) {
+    if (str.length > maxlength)
+        str = str.slice(0, maxlength - 1) + '\u{2026}';
+    return str;
+}
+
+function camelize(str) {
+    let id;
+    while (str.includes("-")) {
+        id = str.indexOf('-');
+        str = str.slice(0, id) + str[id + 1].toUpperCase() + str.slice(id + 2);
+    }
+    return str;
+}
+
+function arrReverseSorted(arr){
+let arr_reserve=[];
+
+arr_reserve=arr.sort((a, b) => b - a);
+
+alert(arr_reserve);
+}
+
+function sum() {
+    let result = 0;
+  
+    for (let i = 0; i < arguments.length; i++) {
+      result += arguments[i];
+    }
+  
+    return result;
+  }
