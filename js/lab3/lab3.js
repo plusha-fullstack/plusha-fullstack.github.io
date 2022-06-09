a2c4678831edb6b54f1449b4b69f36cffeae9bad
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-06-09 19:34:14 +0300

    Update lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -38,9 +38,9 @@ function camelize(str) {
 function fib(n) {
   let a = 1n;
   let b = 1n;
- if (n==0) return 0;
- if (n==1) return 1;
- if (n==2) return 1;
+ if (n==0) return 0n;
+ if (n==1) return 1n;
+ if (n==2) return 1n;
   for (let i = 3; i <= n; i++) {
     let c = a + b;
     a = b;
