206bb60364ef801d79a7ca365916d4b023c7e444
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-06-09 19:28:40 +0300

    Update lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -35,6 +35,26 @@ function camelize(str) {
     }
     return str;
 }
+function fib(n) {
+  let a = 1n;
+  let b = 1n;
+ if (n==0) return 0;
+ if (n==1) return 1;
+ if (n==2) return 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+function fibs (n){
+let arr=[];
+            for (let i = 0; i < n; i++){
+                arr.push( fib(i) );
+            }
+return arr;
+} 
 
 function arrReverseSorted(arr){
 let arr_reserve=[];
